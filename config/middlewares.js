bd12bf696f1c23cbf250e2eb1939c219dbf9ef1f
WSL-Dev1 <167135217+WSL-Dev1@users.noreply.github.com>
2024-05-29 09:25:37 +1200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,3 +10,18 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+
+module.exports = {
+  load: {
+    before: ['cors', 'responseTime', 'logger', 'query', 'body', 'session', 'favicon', 'public'],
+    after: ['parser', 'router'],
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['*'], // Allow all origins, or specify your frontend origin(s) here
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+    },
+  },
+};
