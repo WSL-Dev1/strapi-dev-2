ce55170841693d5431dc09ec75d9d05ec33ca08f
WSL-Dev1 <167135217+WSL-Dev1@users.noreply.github.com>
2024-05-30 10:43:36 +1200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,15 +1,6 @@
 module.exports = ({ env }) => [
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: ['*'], // Update with specific domains in production
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
-    },
-  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
